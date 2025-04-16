cae30e387a0ce6bb210c719435d5f0b3a72b398b
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:12:43 +0630

    -

--- csharp.multitenant/Configurations/AppSetting/AppSetting.cs
+++ csharp.multitenant/Configurations/AppSetting/AppSetting.cs
@@ -1,25 +1,24 @@
-﻿namespace csharp.multitenant.Configurations.AppSetting
+﻿namespace csharp.multitenant.Configurations.AppSetting;
+
+public class AppSetting
 {
-    public class AppSetting
-    {
-        public Logging Logging { get; set; }
-        public Tenant[] Tenants { get; set; }
-    }
+    public Logging Logging { get; set; }
+    public Tenant[] Tenants { get; set; }
+}
 
-    public class Logging
-    {
-        public Loglevel LogLevel { get; set; }
-    }
+public class Logging
+{
+    public Loglevel LogLevel { get; set; }
+}
 
-    public class Loglevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
+public class Loglevel
+{
+    public string Default { get; set; }
+    public string MicrosoftAspNetCore { get; set; }
+}
 
-    public class Tenant
-    {
-        public string TenantId { get; set; }
-        public string ConnectionString { get; set; }
-    }
+public class Tenant
+{
+    public string TenantId { get; set; }
+    public string ConnectionString { get; set; }
 }
