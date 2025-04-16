c6d0ea3985d6387e9fba0b843ff99ef7276e9f48
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:18:16 +0630

    -

--- csharp.multitenant.Models/Features/PageSetting/PageSettingModel.cs
+++ csharp.multitenant.Models/Features/PageSetting/PageSettingModel.cs
@@ -1,10 +1,9 @@
-﻿namespace csharp.multitenant.Models.Features.PageSetting
+﻿namespace csharp.multitenant.Models.Features.PageSetting;
+
+public class PageSettingModel
 {
-    public class PageSettingModel
-    {
-        public int PageNo { get; set; }
-        public int PageSize { get; set; }
-        public int PageCount { get; set; }
-        public int TotalCount { get; set; }
-    }
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public int TotalCount { get; set; }
 }
