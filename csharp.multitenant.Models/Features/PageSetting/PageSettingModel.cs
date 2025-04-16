05f20a12ef062da0b8f4b08b4638e76cc8ddfad0
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:18:12 +0630

    -

--- csharp.multitenant.Models/Features/PageSetting/PageSettingModel.cs
+++ csharp.multitenant.Models/Features/PageSetting/PageSettingModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp.multitenant.Models.Features.PageSetting
+﻿namespace csharp.multitenant.Models.Features.PageSetting
 {
     public class PageSettingModel
     {
