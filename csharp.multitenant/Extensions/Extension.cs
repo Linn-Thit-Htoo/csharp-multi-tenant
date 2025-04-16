b4756bb43010efeffaf4ea3031bbc475a3abf7fc
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:13:09 +0630

    -

--- csharp.multitenant/Extensions/Extension.cs
+++ csharp.multitenant/Extensions/Extension.cs
@@ -1,11 +1,10 @@
 ﻿using Newtonsoft.Json;
 
-namespace csharp.multitenant.Extensions
-{
-    public static class Extension
-    {
-        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+namespace csharp.multitenant.Extensions;
 
-        public static T? ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr);
-    }
+public static class Extension
+{
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+    public static T? ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr);
 }
