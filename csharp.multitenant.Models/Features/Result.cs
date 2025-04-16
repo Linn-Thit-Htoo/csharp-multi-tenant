65b24a30d0db5d6f3e57e687191b40db9c77971e
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:18:26 +0630

    -

--- csharp.multitenant.Models/Features/Result.cs
+++ csharp.multitenant.Models/Features/Result.cs
@@ -1,45 +1,44 @@
 ﻿using System.Net;
 
-namespace csharp.multitenant.Models.Features
+namespace csharp.multitenant.Models.Features;
+
+public class Result<T>
 {
-    public class Result<T>
+    public HttpStatusCode StatusCode { get; set; }
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public T? Data { get; set; }
+
+    public static Result<T> SuccessResult(string message = "Success", HttpStatusCode statusCode = HttpStatusCode.OK)
     {
-        public HttpStatusCode StatusCode { get; set; }
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-        public T? Data { get; set; }
-
-        public static Result<T> SuccessResult(string message = "Success", HttpStatusCode statusCode = HttpStatusCode.OK)
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                IsSuccess = true,
-                Message = message,
-                StatusCode = statusCode,
-                Data = default
-            };
-        }
+            IsSuccess = true,
+            Message = message,
+            StatusCode = statusCode,
+            Data = default
+        };
+    }
 
-        public static Result<T> SuccessResult(T data, string message = "Success", HttpStatusCode statusCode = HttpStatusCode.OK)
+    public static Result<T> SuccessResult(T data, string message = "Success", HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                IsSuccess = true,
-                StatusCode = statusCode,
-                Message = message,
-                Data = data
-            };
-        }
+            IsSuccess = true,
+            StatusCode = statusCode,
+            Message = message,
+            Data = data
+        };
+    }
 
-        public static Result<T> FailureResult(string message = "Fail", HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    public static Result<T> FailureResult(string message = "Fail", HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                Data = default,
-                IsSuccess = false,
-                Message = message,
-                StatusCode = statusCode
-            };
-        }
+            Data = default,
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode
+        };
     }
 }
