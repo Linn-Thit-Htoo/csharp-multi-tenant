dbd490163f4d0b4a3bc5a2120e7bcc5ac758c158
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:13:41 +0630

    -

--- csharp.multitenant/Features/Blog/BlogController.cs
+++ csharp.multitenant/Features/Blog/BlogController.cs
@@ -1,24 +1,23 @@
 ﻿using csharp.multitenant.Features.Core;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp.multitenant.Features.Blog
+namespace csharp.multitenant.Features.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-
-        [HttpGet("GetBlogs")]
-        public async Task<IActionResult> GetBlogs(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var result = await _bL_Blog.GetBlogListAsync(pageNo, pageSize, cs);
-            return Content(result);
-        }
+    [HttpGet("GetBlogs")]
+    public async Task<IActionResult> GetBlogs(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var result = await _bL_Blog.GetBlogListAsync(pageNo, pageSize, cs);
+        return Content(result);
     }
 }
