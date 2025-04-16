4a6204907c667f82b8fbd78d21542247a74e17ea
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:17:37 +0630

    -

--- csharp.multitenant.Models/Features/Blog/BlogModel.cs
+++ csharp.multitenant.Models/Features/Blog/BlogModel.cs
@@ -1,15 +1,14 @@
-﻿namespace csharp.multitenant.Models.Features.Blog
+﻿namespace csharp.multitenant.Models.Features.Blog;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public string BlogId { get; set; } = null!;
+    public string BlogId { get; set; } = null!;
 
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
+    public string BlogContent { get; set; } = null!;
 
-        public bool IsDeleted { get; set; }
-    }
+    public bool IsDeleted { get; set; }
 }
