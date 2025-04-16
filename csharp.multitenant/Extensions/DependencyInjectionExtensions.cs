fb4bc0a19ec4b32bc6af7dc25c7fad738fe0b16a
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:12:57 +0630

    -

--- csharp.multitenant/Extensions/DependencyInjectionExtensions.cs
+++ csharp.multitenant/Extensions/DependencyInjectionExtensions.cs
@@ -4,67 +4,66 @@ using csharp.multitenant.Configurations.AppSetting;
 using csharp.multitenant.Features.Blog;
 using Microsoft.EntityFrameworkCore;
 
-namespace csharp.multitenant.Extensions
+namespace csharp.multitenant.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        return builder.Services
+            .AddCommonServices(builder)
+            .AddDbContextService(builder)
+            .AddBusinessLogicServices()
+            .AddDataAccessServices();
+    }
+
+    public static IServiceCollection AddCommonServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            return builder.Services
-                .AddCommonServices(builder)
-                .AddDbContextService(builder)
-                .AddBusinessLogicServices()
-                .AddDataAccessServices();
-        }
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-        public static IServiceCollection AddCommonServices(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
+
+        builder.Services.AddTransient<TenantProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>((sp, opt) =>
         {
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+            var tenantProvider = sp.GetRequiredService<TenantProvider>();
+            string connectionString = tenantProvider.GetConnectionString();
 
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.UseSqlServer(connectionString);
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
+        return services;
+    }
 
-            builder.Services.AddTransient<TenantProvider>();
+    public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
 
-            return services;
-        }
+        return services;
+    }
 
-        public static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>((sp, opt) =>
-            {
-                var tenantProvider = sp.GetRequiredService<TenantProvider>();
-                string connectionString = tenantProvider.GetConnectionString();
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
 
-                opt.UseSqlServer(connectionString);
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
-
-            return services;
-        }
-
-        public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>();
-
-            return services;
-        }
-
-        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Blog>();
-
-            return services;
-        }
+        return services;
     }
 }
