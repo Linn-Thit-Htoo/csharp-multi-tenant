3f45864a8693dbad4e28757893dd56e174d947ae
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:17:25 +0630

    -

--- csharp.multitenant.Models/Features/Blog/BlogListResponseModelV1.cs
+++ csharp.multitenant.Models/Features/Blog/BlogListResponseModelV1.cs
@@ -1,10 +1,9 @@
 ﻿using csharp.multitenant.Models.Features.PageSetting;
 
-namespace csharp.multitenant.Models.Features.Blog
+namespace csharp.multitenant.Models.Features.Blog;
+
+public class BlogListResponseModelV1
 {
-    public class BlogListResponseModelV1
-    {
-        public List<BlogModel> Blogs { get; set; }
-        public PageSettingModel PageSetting { get; set; }
-    }
+    public List<BlogModel> Blogs { get; set; }
+    public PageSettingModel PageSetting { get; set; }
 }
