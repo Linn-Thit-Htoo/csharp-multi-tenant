ff4959841ff34afca998ea36a99d730b6b7d073d
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:16:42 +0630

    -

--- csharp.multitenant/Features/Blog/DA_Blog.cs
+++ csharp.multitenant/Features/Blog/DA_Blog.cs
@@ -6,60 +6,59 @@ using csharp.multitenant.Models.Features.PageSetting;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
-namespace csharp.multitenant.Features.Blog
+namespace csharp.multitenant.Features.Blog;
+
+public class DA_Blog
 {
-    public class DA_Blog
+    private readonly AppDbContext _context;
+    private readonly ILogger<DA_Blog> _logger;
+
+    public DA_Blog(AppDbContext context, ILogger<DA_Blog> logger)
     {
-        private readonly AppDbContext _context;
-        private readonly ILogger<DA_Blog> _logger;
+        _context = context;
+        _logger = logger;
+    }
 
-        public DA_Blog(AppDbContext context, ILogger<DA_Blog> logger)
+    public async Task<Result<BlogListResponseModelV1>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs = default)
+    {
+        Result<BlogListResponseModelV1> result;
+        try
         {
-            _context = context;
-            _logger = logger;
-        }
+            var query = _context.TblBlogs.Where(x => !x.IsDeleted);
+            var lst = await query
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken: cs);
 
-        public async Task<Result<BlogListResponseModelV1>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs = default)
-        {
-            Result<BlogListResponseModelV1> result;
-            try
+            int totalCount = await query.CountAsync(cs);
+            int pageCount = totalCount / pageSize;
+
+            if (totalCount % pageSize > 0)
             {
-                var query = _context.TblBlogs.Where(x => !x.IsDeleted);
-                var lst = await query
-                    .Skip((pageNo - 1) * pageSize)
-                    .Take(pageSize)
-                    .ToListAsync(cancellationToken: cs);
-
-                int totalCount = await query.CountAsync(cs);
-                int pageCount = totalCount / pageSize;
-
-                if (totalCount % pageSize > 0)
-                {
-                    pageCount++;
-                }
-
-                var pageSetting = new PageSettingModel()
-                {
-                    PageCount = pageCount,
-                    PageNo = pageNo,
-                    PageSize = pageSize,
-                    TotalCount = totalCount,
-                };
-                var model = new BlogListResponseModelV1()
-                {
-                    PageSetting = pageSetting,
-                    Blogs = lst.Select(x => x.ToDto()).ToList()
-                };
-
-                result = Result<BlogListResponseModelV1>.SuccessResult(model);
-            }
-            catch (Exception ex)
-            {
-                result = Result<BlogListResponseModelV1>.FailureResult(ex.ToString(), HttpStatusCode.InternalServerError);
+                pageCount++;
             }
 
-        result:
-            return result;
+            var pageSetting = new PageSettingModel()
+            {
+                PageCount = pageCount,
+                PageNo = pageNo,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+            };
+            var model = new BlogListResponseModelV1()
+            {
+                PageSetting = pageSetting,
+                Blogs = lst.Select(x => x.ToDto()).ToList()
+            };
+
+            result = Result<BlogListResponseModelV1>.SuccessResult(model);
         }
+        catch (Exception ex)
+        {
+            result = Result<BlogListResponseModelV1>.FailureResult(ex.ToString(), HttpStatusCode.InternalServerError);
+        }
+
+    result:
+        return result;
     }
 }
