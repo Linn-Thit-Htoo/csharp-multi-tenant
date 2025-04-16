1b11e59c2e41b34a13a4db2c8466f71b4b669085
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:12:33 +0630

    -

--- csharp.multitenant/Behaviors/TenantProvider.cs
+++ csharp.multitenant/Behaviors/TenantProvider.cs
@@ -1,23 +1,22 @@
 ﻿using csharp.multitenant.Configurations.AppSetting;
 using Microsoft.Extensions.Options;
 
-namespace csharp.multitenant.Behaviors
+namespace csharp.multitenant.Behaviors;
+
+public class TenantProvider
 {
-    public class TenantProvider
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly AppSetting _appSetting;
+
+    public TenantProvider(IHttpContextAccessor httpContextAccessor, IOptions<AppSetting> setting)
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly AppSetting _appSetting;
-
-        public TenantProvider(IHttpContextAccessor httpContextAccessor, IOptions<AppSetting> setting)
-        {
-            _httpContextAccessor = httpContextAccessor;
-            _appSetting = setting.Value;
-        }
-
-        public string TenantId => _httpContextAccessor.HttpContext!
-            .Request.Headers[Constants.ApplicationConstants.TenantHeader].ToString();
-
-        public string GetConnectionString() => _appSetting.Tenants
-            .FirstOrDefault(x => x.TenantId == TenantId)?.ConnectionString!;
+        _httpContextAccessor = httpContextAccessor;
+        _appSetting = setting.Value;
     }
+
+    public string TenantId => _httpContextAccessor.HttpContext!
+        .Request.Headers[Constants.ApplicationConstants.TenantHeader].ToString();
+
+    public string GetConnectionString() => _appSetting.Tenants
+        .FirstOrDefault(x => x.TenantId == TenantId)?.ConnectionString!;
 }
