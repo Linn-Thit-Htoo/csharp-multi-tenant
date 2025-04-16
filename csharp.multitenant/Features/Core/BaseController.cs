091ee57dfae3eb9c4e046f5604b8250c303d2ebe
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:16:58 +0630

    -

--- csharp.multitenant/Features/Core/BaseController.cs
+++ csharp.multitenant/Features/Core/BaseController.cs
@@ -1,15 +1,14 @@
 ﻿using csharp.multitenant.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp.multitenant.Features.Core
+namespace csharp.multitenant.Features.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
