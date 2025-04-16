3903bbc4562375aeb94eef0b6b63348ec1e91cc4
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:12:17 +0630

    -

--- csharp.multitenant/AppDbContextModels/AppDbContext.cs
+++ csharp.multitenant/AppDbContextModels/AppDbContext.cs
@@ -5,9 +5,7 @@ namespace csharp.multitenant.AppDbContextModels;
 public partial class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public virtual DbSet<TblBlog> TblBlogs { get; set; }
 
