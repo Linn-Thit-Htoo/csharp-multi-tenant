f17945633d738f8fdf0bb641dee21c2cbf6ce315
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-16 09:13:18 +0630

    -

--- csharp.multitenant/Extensions/Mapper.cs
+++ csharp.multitenant/Extensions/Mapper.cs
@@ -1,20 +1,19 @@
 ﻿using csharp.multitenant.AppDbContextModels;
 using csharp.multitenant.Models.Features.Blog;
 
-namespace csharp.multitenant.Extensions
+namespace csharp.multitenant.Extensions;
+
+public static class Mapper
 {
-    public static class Mapper
+    public static BlogModel ToDto(this TblBlog model)
     {
-        public static BlogModel ToDto(this TblBlog model)
+        return new BlogModel
         {
-            return new BlogModel
-            {
-                BlogId = model.BlogId,
-                BlogTitle = model.BlogTitle,
-                BlogAuthor = model.BlogAuthor,
-                BlogContent = model.BlogContent,
-                IsDeleted = model.IsDeleted
-            };
-        }
+            BlogId = model.BlogId,
+            BlogTitle = model.BlogTitle,
+            BlogAuthor = model.BlogAuthor,
+            BlogContent = model.BlogContent,
+            IsDeleted = model.IsDeleted
+        };
     }
 }
